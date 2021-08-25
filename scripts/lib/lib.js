96d3c0ea4bce546e1fce4759abe5617aeb747bf0
Philipp Schaefer <PhSchaefer@web.de>
2021-08-25 19:51:15 +0200

    Included Target Lang Dropdown, but not functional.

--- scripts/lib/lib.js
+++ scripts/lib/lib.js
@@ -6,7 +6,8 @@ class ComprehendLanguages {
     }
 
     static SETTINGS = {
-        DEEPL_TOKEN : 'deepl-token'
+        DEEPL_TOKEN : 'deepl-token',
+        TARGET_LANG : 'target-language'
     }
 
     static log(force, ...args) {  
@@ -25,6 +26,40 @@ class ComprehendLanguages {
             hint: "Insert your DeepL Token here",
             type: String,
             default: '',
+            scope: "world"
+        });
+        game.settings.register(this.ID, this.SETTINGS.TARGET_LANG,{
+            name: 'Target Language',
+            config: true,
+            hint: "What should your target language be",
+            type: String,
+            default: 'DE',
+            choices: {                
+                "BG" : "Bulgarian",
+                "CS" : "Czech",
+                "DA" : "Danish",
+                "DE" : "German",
+                "EL" : "Greek",
+                "EN" : "English",
+                "ES" : "Spanish",
+                "ET" : "Estonian",
+                "FI" : "Finnish",
+                "FR" : "French",
+                "HU" : "Hungarian",
+                "IT" : "Italian",
+                "JA" : "Japanese",
+                "LT" : "Lithuanian",
+                "LV" : "Latvian",
+                "NL" : "Dutch",
+                "PL" : "Polish",
+                "PT" : "Portuguese (all Portuguese varieties mixed)",
+                "RO" : "Romanian",
+                "RU" : "Russian",
+                "SK" : "Slovak",
+                "SL" : "Slovenian",
+                "SV" : "Swedish",
+                "ZH" : "Chinese",
+            },
             scope: "world"
         })
     }
